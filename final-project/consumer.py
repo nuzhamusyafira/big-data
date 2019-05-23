a62e8d8e2ddce1687ef38cf813fa1fc98af440a8
nuzhamusyafira <nuzhamusyafira@gmail.com>
2019-05-23 16:50:41 +0700

    initiate kafka server

---
+++ final-project/consumer.py
@@ -0,0 +1,39 @@
+from kafka import KafkaConsumer
+from json import loads
+import os
+
+consumer = KafkaConsumer(
+    'denver-crime',
+     bootstrap_servers=['localhost:9092'],
+     auto_offset_reset='earliest',
+     enable_auto_commit=True,
+     value_deserializer=lambda x: loads(x.decode('utf-8')))
+
+folder_path = os.path.join(os.getcwd(), 'dataset-kafka')
+limit = 50
+row = 1
+model = 1
+model_limit = 3
+try:
+    for message in consumer:
+        if model > model_limit:
+            writefile.close()
+            break
+        else:
+            if row > model_limit:
+                row = 1
+                model += 1
+                writefile.close()
+            if model > model_limit:
+                writefile.close()
+                break
+            if row == 1:
+                file_path = os.path.join(folder_path, ('model-' + str(model) + '.txt'))
+                writefile = open(file_path, "w", encoding="utf-8")
+            message = message.value
+            writefile.write(message)
+            print('current batch : ' + str(model) + ' current data for this batch : ' + str(row))
+            row += 1
+except KeyboardInterrupt:
+    writefile.close()
+    print('Keyboard Interrupt called by user, exiting.....')
---
+++ final-project/producer.py
@@ -0,0 +1,24 @@
+from time import sleep
+from json import dumps
+from kafka import KafkaProducer
+import os
+import pandas as pd
+
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                         value_serializer=lambda x:
+                         dumps(x).encode('utf-8'))
+
+#Load Dataset
+dataset_folder_path = os.path.join(os.getcwd(), 'dataset')
+dataset_file_path = os.path.join(dataset_folder_path, 'crime_preprocessed.csv')
+model = 3
+limit = 50
+counter = 0
+with open(dataset_file_path,"r", encoding="utf-8") as f:
+    for row in f:
+        if counter > model*limit:
+            break
+        producer.send('denver-crime', value=row)
+        counter += 1
+        print(row)
+        sleep(0.0000001)
