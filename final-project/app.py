8427050a4433f7530b24485751e45ae5723f4464
nuzhamusyafira <nuzhamusyafira@gmail.com>
2019-05-23 21:47:59 +0700

    add api

--- final-project/app.py
+++ final-project/app.py
@@ -16,19 +16,19 @@ from flask import Flask, request
 @main.route("/model1/<int:crime_id>/cluster", methods=["GET"])
 def get_cluster1(crime_id):
     logger.debug("ID %s requested", crime_id)
-    cluster_category = clustering_engine.get_cluster(crime_id)
+    cluster_category = clustering_engine.get_cluster1(crime_id)
     return json.dumps(cluster_category)
 
 @main.route("/model2/<int:crime_id>/cluster", methods=["GET"])
 def get_cluster2(crime_id):
     logger.debug("ID %s requested", crime_id)
-    cluster_category = clustering_engine.get_cluster(crime_id)
+    cluster_category = clustering_engine.get_cluster2(crime_id)
     return json.dumps(cluster_category)
 
 @main.route("/model3/<int:crime_id>/cluster", methods=["GET"])
 def get_cluster3(crime_id):
     logger.debug("ID %s requested", crime_id)
-    cluster_category = clustering_engine.get_cluster(crime_id)
+    cluster_category = clustering_engine.get_cluster3(crime_id)
     return json.dumps(cluster_category)
 
 def create_app(spark_session, dataset_path):
---
+++ final-project/engine.py
@@ -0,0 +1,107 @@
+import os
+import logging
+import pandas as pd
+
+from pyspark.ml.evaluation import RegressionEvaluator
+from pyspark.ml.recommendation import ALS
+from pyspark.sql import Row
+from pyspark.sql import types
+from pyspark.sql.functions import explode
+import pyspark.sql.functions as func
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+class ClusteringEngine:
+    """A movie recommendation engine
+    """
+
+    def __transform_model(self):
+        """Train the ALS model with the current dataset
+        """
+        assembler = VectorAssembler(inputCols=["GEO_LAT", "GET_LON"], outputCol='features')
+        self.df_crime1 = assembler.transform(self.df_crime1)
+
+        assembler = VectorAssembler(inputCols=["GEO_LAT", "GET_LON"], outputCol='features')
+        self.df_crime2 = assembler.transform(self.df_crime2)
+
+        assembler = VectorAssembler(inputCols=["GEO_LAT", "GET_LON"], outputCol='features')
+        self.df_crime3 = assembler.transform(self.df_crime3)
+
+
+    def __train_model(self):
+        """Train the ALS model with the current dataset
+        """
+        logger.info("Training model 1...")
+        kmeans_1 = KMeans().setK(9).setSeed(1)
+        model_1 = kmeans_1.fit(self.df_crime1)
+        logger.info("Model 1 built!")
+        logger.info("Evaluating the model 1...")
+        self.centers_1 = model_1.clusterCenters()
+        logger.info("Model 1 Done !")
+
+        logger.info("Training model 2...")
+        kmeans_2 = KMeans().setK(9).setSeed(1)
+        model_2 = kmeans_2.fit(self.df_crime2)
+        logger.info("Model 2 built!")
+        logger.info("Evaluating the model 2...")
+        self.centers_2 = model_2.clusterCenters()
+        logger.info("Model 2 Done !")
+
+        logger.info("Training model 3...")
+        kmeans_3 = KMeans().setK(9).setSeed(1)
+        model_3 = kmeans_3.fit(self.df_crime3)
+        logger.info("Model 3 built!")
+        logger.info("Evaluating the model 3...")
+        self.centers_3 = model_3.clusterCenters()
+        logger.info("Model 3 Done !")
+
+
+    def get_cluster1(self, crime_id):
+        """Recommends up to movies_count top unrated movies to user_id
+        """
+        
+        
+    def get_cluster2(self, crime_id):
+        """Recommends up to movies_count top unrated movies to user_id
+        """
+        
+        
+    def get_cluster3(self, crime_id):
+        """Recommends up to movies_count top unrated movies to user_id
+        """
+        
+
+    def __init__(self, spark_session, dataset_path):
+        """Init the recommendation engine given a Spark context and a dataset path
+        """
+        logger.info("Starting up the Clustering Engine: ")
+        self.spark_session = spark_session
+        # Load ratings data for later use
+        logger.info("Loading Crimes data...")
+        file_name1 = 'model-1.txt'
+        dataset_file_path1 = os.path.join(dataset_path, file_name1)
+        exist = os.path.isfile(dataset_file_path1)
+        if exist:
+            self.df_crime1 = spark_session.read.csv(dataset_file_path1, header=None, inferSchema=True)
+            self.df_crime1 = self.df_crime1.selectExpr("_c0 as INCIDENT_ID", "_c1 as GEO_LAT", "_c2 as GET_LON")
+
+        file_name2 = 'model-2.txt'
+        dataset_file_path2 = os.path.join(dataset_path, file_name2)
+        exist = os.path.isfile(dataset_file_path2)
+        if exist:
+            self.df_crime2 = spark_session.read.csv(dataset_file_path2, header=None, inferSchema=True)
+            self.df_crime2 = self.df_crime2.selectExpr("_c0 as INCIDENT_ID", "_c1 as GEO_LAT", "_c2 as GET_LON")
+
+        file_name3 = 'model-3.txt'
+        dataset_file_path3 = os.path.join(dataset_path, file_name3)
+        exist = os.path.isfile(dataset_file_path3)
+        if exist:
+            self.df_crime3 = spark_session.read.csv(dataset_file_path3, header=None, inferSchema=True)
+            self.df_crime3 = self.df_crime3.selectExpr("_c0 as INCIDENT_ID", "_c1 as GEO_LAT", "_c2 as GET_LON")
+
+
+        # Train the model
+        self.__transform_model()
+        self.__train_model()
