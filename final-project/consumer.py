742a82f4d77300b1e14a98efc2f7636c253af756
nuzhamusyafira <nuzhamusyafira@gmail.com>
2019-05-23 16:59:08 +0700

    fix conflict

--- final-project/consumer.py
+++ final-project/consumer.py
@@ -10,30 +10,30 @@ consumer = KafkaConsumer(
      value_deserializer=lambda x: loads(x.decode('utf-8')))
 
 folder_path = os.path.join(os.getcwd(), 'dataset-kafka')
-limit = 50
-row = 1
-model = 1
-model_limit = 3
+batch_limit = 50
+batch_counter = 1
+batch_number = 1
+batch_len = 3
 try:
     for message in consumer:
-        if model > model_limit:
+        if batch_number > batch_len:
             writefile.close()
             break
         else:
-            if row > model_limit:
-                row = 1
-                model += 1
+            if batch_counter > batch_limit:
+                batch_counter = 1
+                batch_number += 1
                 writefile.close()
-            if model > model_limit:
+            if batch_number > batch_len:
                 writefile.close()
                 break
-            if row == 1:
-                file_path = os.path.join(folder_path, ('model-' + str(model) + '.txt'))
+            if batch_counter == 1:
+                file_path = os.path.join(folder_path, ('model-' + str(batch_number) + '.txt'))
                 writefile = open(file_path, "w", encoding="utf-8")
             message = message.value
             writefile.write(message)
-            print('current batch : ' + str(model) + ' current data for this batch : ' + str(row))
-            row += 1
+            print('current batch : ' + str(batch_number) + ' current data for this batch : ' + str(batch_counter))
+            batch_counter += 1
 except KeyboardInterrupt:
     writefile.close()
     print('Keyboard Interrupt called by user, exiting.....')
